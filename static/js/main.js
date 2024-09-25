402948c7a899ccf448ba075ee9e654a229c326f5
Megan Roberts <megan.29@icloud.com>
2024-09-25 09:35:29 +0000

    Fix login and register bug

--- static/js/main.js
+++ static/js/main.js
@@ -1,22 +1,24 @@
-    // Array of motivational quotes
-    var quotes = [
-        "Believe in yourself and all that you are.",
-        "The only way to achieve the impossible is to believe it is possible.",
-        "Success is not the key to happiness. Happiness is the key to success.",
-        "Don't watch the clock; do what it does. Keep going.",
-        "You are never too old to set another goal or to dream a new dream.",
-        "Start where you are. Use what you have. Do what you can."
-    ];
+// Array of motivational quotes
+var quotes = [
+    "Believe in yourself and all that you are.",
+    "The only way to achieve the impossible is to believe it is possible.",
+    "Success is not the key to happiness. Happiness is the key to success.",
+    "Don't watch the clock; do what it does. Keep going.",
+    "You are never too old to set another goal or to dream a new dream.",
+    "Start where you are. Use what you have. Do what you can."
+];
 
-    function changeTitle() {
-        var titleElement = document.querySelector('#motivational-header');
-        var randomIndex = Math.floor(Math.random() * quotes.length);
-        titleElement.textContent = quotes[randomIndex];
-    }
+function changeTitle() {
+    var randomIndex = Math.floor(Math.random() * quotes.length);
+    titleElement.textContent = quotes[randomIndex];
+}
 
-    // Change the title every 5 seconds
+// Change the title every 5 seconds
+var titleElement = document.querySelector('#motivational-header');
+if (titleElement) {
     setInterval(changeTitle, 5000);
     changeTitle();
+}
 
 function toggleIcon(label) {
     const checkbox = label.previousElementSibling; // Get the checkbox
@@ -28,8 +30,8 @@ function toggleIcon(label) {
         label.innerHTML = '<i class="bi bi-circle check-icon"></i>';
     }
 }
-document.addEventListener('DOMContentLoaded', function() {
-    setTimeout(function() {
+document.addEventListener('DOMContentLoaded', function () {
+    setTimeout(function () {
         let flashMessage = document.querySelector('.flash-banner');
         if (flashMessage) {
             flashMessage.style.transition = "opacity 0.5s ease-out";
